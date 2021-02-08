1d6c3de0f7f317aa41774d9e38b08f2be0c9573d
Nacho <Nacho@LAPTOP-00RLUILC>
2021-02-08 02:09:40 +0100

    Añadido rss

--- view/layout.php
+++ view/layout.php
@@ -21,6 +21,7 @@
                 <a href="https://github.com/NachoPLSauces" target="_blank"><img src="./webroot/media/img/github-icon.png" alt="github"></a>
                 <a href="http://daw202.ieslossauces.es/" target="_blank"><img src="./webroot/media/img/1and1-icon.png" alt="1n1"></a>
                 <a href="./doc/phpDoc/index.html" target="_blank"><img src="./webroot/media/img/phpDocLogo.png" alt="phpDoc"></a>
+                <a href="./doc/rss.xml" target="_blank"><img src="./webroot/media/img/rss.png" alt="rss"></a>
                 <a href="../../index.html"><img src="./webroot/media/img/home.png" alt="npl"></a>
             </div>
             <div class="nombre">
